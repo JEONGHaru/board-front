ec8e7f99b72569c50783cee0f4f01cbadd6037cb
jeong kwangyoung <holyanegl@gmail.com>
2024-10-18 02:00:41 +0900

    Create Component Top3 Item

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
-import BoardItem from 'components/BoardItem';
-import latestBoardListMock from 'mocks/latest-board-list.mock';
+import top3BoardListItem from 'mocks/top-3-board-list.mock';
+import Top3Item from 'components/Top3Item';
 
 function App() {
   return (
     <>
-      {latestBoardListMock.map(boardListItem => <BoardItem boardListItem={boardListItem}/>)}
+      <div style={{ display: 'flex', justifyContent: 'center', gap: '24px'}}>
+        {top3BoardListItem.map(top3ListItem => <Top3Item top3ListItem={top3ListItem} />)}
+      </div>
       
     </>
   );
